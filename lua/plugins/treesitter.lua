5e94efd5dd6baef558489c01210dfde43ac5c90b
neerajgurung2099 <neerajgurung2099@gmail.com>
2026-01-15 21:18:43 +0000

    Update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,5 @@
 return { 
-   'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc'  ,'javascript', 'typescript' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    }
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	lazy = false , 
 }
