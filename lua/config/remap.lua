c9da9457b0db6b06d6488b002a00e9326a08382c
neerajgurung2099 <neerajgurung2099@gmail.com>
2026-01-15 23:59:33 +0000

    Update

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>nv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>bd", ":%bd<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set("v" , "J" , ":m '>+1<CR>gv=gv")
+vim.keymap.set("v" , "K" , ":m '<-2<CR>gv=gv")
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -6,4 +6,17 @@ vim.o.undofile = true
 vim.o.scrolloff = 10 
 vim.o.confirm =true
 
+vim.o.tabstop=4 
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true 
+vim.o.smartindent =true 
+
+vim.api.nvim_create_autocmd('TextYankPost' , { 
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
 
